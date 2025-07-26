455cb041670c87c18a1f96af920586de8052bd15
Danny7S <danielsl@abv.bg>
2025-07-27 01:42:21 +0300

    Initial setup for Task API with Task entity, DbContext, and basic CRUD structure

---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Task
+{
+    [Key]
+    public int TaskId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime ModifiedAt { get; set; }
+    public bool Approved { get; set; }
+}
---
+++ TaskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskApi
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext>options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<TaskApi.Models.Task> Tasks { get; set; }
+    }
+}
